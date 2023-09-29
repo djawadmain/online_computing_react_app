5b7fd4dfbf9ad1da3e013b539fa232c225bf0a7f
djawad <djawad.main@gmail.com>
2023-09-29 22:03:10 +0330

    only for push

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Cross from "./SvgComponents/Cross";
 
 export default class Result extends React.Component {
 
@@ -15,11 +16,7 @@ export default class Result extends React.Component {
     render() {
         return <div id="result" className="absolute bg-gray-100 z-20 rounded-lg overflow-y-auto h-full p-2">
             <button id="close_result" className="p-2.5 mb-1.5 bg-gray-700 text-gray-200 rounded-lg" type="button">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5"
-                     stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/>
-                </svg>
-
+                <Cross />
             </button>
             <span className="break-words text-xs" id="result_number">
                 {this.state.number_generated}
